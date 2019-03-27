8dd2616077ba93d56540f68915abb6146275a692
Hanbin Sock <hbsock@gmail.com>
2019-03-26 22:46:29 -0400

    Get working example of http-conduit

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,6 +1,22 @@
+{-# LANGUAGE OverloadedStrings #-}
+
 module Lib
     ( someFunc
     ) where
 
+
+import qualified Data.ByteString.Lazy.Char8 as L8
+
+import Network.HTTP.Simple
+
 someFunc :: IO ()
-someFunc = putStrLn "someFunc"
+someFunc = do
+    response <- httpLBS "http://httpbin.org/get"
+
+    putStrLn $ "Status code was: " ++ 
+                show (getResponseStatusCode response)
+    print $ getResponseHeader "Content-Type" response
+    L8.putStrLn $ getResponseBody response
+
+-- someFunc :: IO ()
+-- someFunc = putStrLn "someFunc"
