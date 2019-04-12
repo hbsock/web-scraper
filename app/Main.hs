5500a1db482947ed451a9197b368e7a4ab7c6762
Hanbin Sock <hbsock@gmail.com>
2019-04-11 21:33:31 -0400

    Get read text file into all uppercase

--- app/Main.hs
+++ app/Main.hs
@@ -4,10 +4,14 @@ module Main where
 -- import Parser
 import System.IO
 import qualified Data.ByteString.Lazy.Char8 as B
+import Data.Char (toUpper)
 
 
 main :: IO ()
 main = do
     file <- B.readFile "thing.html"
-    print file
--- main = someFunc
+    let lines = B.lines file
+    let string_lines = map B.unpack lines
+    let uppLines = map toUpper $ unlines string_lines
+
+    print uppLines 
