da98a61403430665010ab0d9d4c593182c54fd59
Hanbin Sock <hbsock@gmail.com>
2019-04-11 21:17:02 -0400

    Add reading file as lazy char8 bytestring.

--- app/Main.hs
+++ app/Main.hs
@@ -1,8 +1,13 @@
 module Main where
 
-import Lib
-
+-- import Lib
+-- import Parser
+import System.IO
+import qualified Data.ByteString.Lazy.Char8 as B
 
 
 main :: IO ()
-main = someFunc
+main = do
+    file <- B.readFile "thing.html"
+    print file
+-- main = someFunc
--- src/Lib.hs
+++ src/Lib.hs
@@ -6,17 +6,21 @@ module Lib
 
 
 import qualified Data.ByteString.Lazy.Char8 as L8
-
 import Network.HTTP.Simple
+
+import Control.Monad.IO.Class (MonadIO)
+
+
+-- general_http :: MonadIO m => String -> m (Response L8.ByteString)
+-- general_http verb = do
+--     initReq <- parseRequest ("http://httpbin.org/" ++ verb)
+--     httpLBS initReq
 
 someFunc :: IO ()
 someFunc = do
-    response <- httpLBS "http://httpbin.org/get"
+    response <- httpLBS "https://boxnovel.com/novel/chaotic-sword-god/chapter-2038"
 
     putStrLn $ "Status code was: " ++ 
                 show (getResponseStatusCode response)
     print $ getResponseHeader "Content-Type" response
     L8.putStrLn $ getResponseBody response
-
--- someFunc :: IO ()
--- someFunc = putStrLn "someFunc"
