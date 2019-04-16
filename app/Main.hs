38a593ccf267edf82cede4397099a920667249cd
Hanbin Sock <hbsock@gmail.com>
2019-04-15 21:59:52 -0400

    Properly parse for chapter contents.

--- app/Main.hs
+++ app/Main.hs
@@ -3,15 +3,11 @@ module Main where
 -- import Lib
 -- import Parser
 import System.IO
-import qualified Data.ByteString.Lazy.Char8 as B
-import Data.Char (toUpper)
-
+import Text.HTML.TagSoup
 
 main :: IO ()
 main = do
-    file <- B.readFile "thing.html"
-    let lines = B.lines file
-    let string_lines = map B.unpack lines
-    let uppLines = map toUpper $ unlines string_lines
+    file <- readFile "thing.html"
+    let chapter_contents = takeWhile (~/= "</div>") . dropWhile (~/= "<div class=\"text-left\">") . parseTags $ file
 
-    print uppLines 
+    print chapter_contents
