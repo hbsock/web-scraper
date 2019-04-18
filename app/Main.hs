02533fc90fe4ab8cf9c8b672e9926f3aca6f1fcf
Hanbin Sock <hbsock@gmail.com>
2019-04-17 23:15:04 -0400

    Refactor the parsing of html file into separate file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,13 @@
 module Main where
 
 -- import Lib
--- import Parser
+import Parser (parseBoxNovelHTML)
 import System.IO
-import Text.HTML.TagSoup
-import Data.Maybe (catMaybes)
-
-parseForChapterContents :: [Tag String] -> [Tag String]
-parseForChapterContents = takeWhile (~/= "</div>") . dropWhile (~/= "<div class=\"text-left\">")
 
 main :: IO ()
 main = do
     file <- readFile "thing.html"
-    let chapter_contents = catMaybes . map maybeTagText . parseForChapterContents . parseTags $ file
+    let chapter_contents = parseBoxNovelHTML file
 
     writeFile "out.txt" $ unlines chapter_contents
-    
     print chapter_contents
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,3 +1,13 @@
-module Parser where
+module Parser 
+    ( parseBoxNovelHTML
+    ) where
 
 
+import Text.HTML.TagSoup
+import Data.Maybe (catMaybes)
+
+parseForChapterContents :: [Tag String] -> [Tag String]
+parseForChapterContents = takeWhile (~/= "</div>") . dropWhile (~/= "<div class=\"text-left\">")
+
+parseBoxNovelHTML :: String -> [String]
+parseBoxNovelHTML = catMaybes . map maybeTagText . parseForChapterContents . parseTags
