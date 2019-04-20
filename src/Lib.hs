e33665979f7e9a4983bce58013cb125326d57789
Hanbin Sock <hbsock@gmail.com>
2019-04-20 19:16:34 -0400

    Remove old someFunc function.

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,7 +1,7 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( someFunc,
+    ( 
       getWebsiteWithDelay
     ) where
 
@@ -9,7 +9,6 @@ module Lib
 import qualified Data.Text as T
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
-import Data.Text.Encoding (decodeUtf8)
 import System.Random (getStdRandom, randomR)
 import Control.Concurrent (threadDelay)
 
@@ -27,23 +26,3 @@ getWebsiteWithDelay path = do
     response <- httpLBS =<< parseRequest ("https://en.wikipedia.org/wiki/" ++ path)
 
     return $ getResponseBody response
-
-    
-    
-
-someFunc :: IO ()
-someFunc = do
-
-    rand_delay_in_microsec <- getRandomNumInRange 1 100000
-    threadDelay rand_delay_in_microsec
-    putStrLn $ "Delayed for " ++ show (rand_delay_in_microsec `div` 1000) ++ " miliseconds."
-    
-    {--
-     -
-    response <- httpLBS =<< parseRequest ("https://en.wikipedia.org/wiki/" ++ "Anime")
-    putStrLn $ "Status code was: " ++ show (getResponseStatusCode response)
-    print $ getResponseHeader "Content-Type" response
-    --L8.writeFile "wiki.html" $ getResponseBody response
-    --}
-
-    
