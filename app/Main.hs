925be45e9d8cadfe7fc829aba73ef8788339b6fb
Hanbin Sock <hbsock@gmail.com>
2019-04-20 19:15:42 -0400

    Implement getting website contents after a delay in time.

--- app/Main.hs
+++ app/Main.hs
@@ -2,9 +2,13 @@ module Main where
 
 
 import Parser (parseMain)
-import Lib (someFunc)
+import Lib (getWebsiteWithDelay)
 import System.IO
+import qualified Data.Text.Lazy.IO as I
+import Data.Text.Lazy.Encoding (decodeUtf8)
 
 
 main :: IO ()
-main = someFunc
+main = do
+    contents <- getWebsiteWithDelay "Anime"
+    I.writeFile "wiki.html" (decodeUtf8 contents)
--- src/Lib.hs
+++ src/Lib.hs
@@ -1,12 +1,15 @@
 {-# LANGUAGE OverloadedStrings #-}
 
 module Lib
-    ( someFunc
+    ( someFunc,
+      getWebsiteWithDelay
     ) where
 
 
+import qualified Data.Text as T
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
+import Data.Text.Encoding (decodeUtf8)
 import System.Random (getStdRandom, randomR)
 import Control.Concurrent (threadDelay)
 
@@ -15,14 +18,28 @@ getRandomNumInRange :: Int -> Int -> IO Int
 getRandomNumInRange lo hi = getStdRandom (randomR (lo, hi))
 
 
+getWebsiteWithDelay :: String -> IO L8.ByteString
+getWebsiteWithDelay path = do
+    rand_delay_in_microsec <- getRandomNumInRange 1 100000
+    threadDelay rand_delay_in_microsec
+    putStrLn $ "Delayed for " ++ show (rand_delay_in_microsec `div` 1000) ++ " miliseconds."
+    
+    response <- httpLBS =<< parseRequest ("https://en.wikipedia.org/wiki/" ++ path)
+
+    return $ getResponseBody response
+
+    
+    
+
 someFunc :: IO ()
 someFunc = do
 
-    rand_delay_in_microsec <- getRandomNumInRange 1 3000000
+    rand_delay_in_microsec <- getRandomNumInRange 1 100000
     threadDelay rand_delay_in_microsec
     putStrLn $ "Delayed for " ++ show (rand_delay_in_microsec `div` 1000) ++ " miliseconds."
     
     {--
+     -
     response <- httpLBS =<< parseRequest ("https://en.wikipedia.org/wiki/" ++ "Anime")
     putStrLn $ "Status code was: " ++ show (getResponseStatusCode response)
     print $ getResponseHeader "Content-Type" response
