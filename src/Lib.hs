5331475494580676f4879c25082e08e1e51c27c3
Hanbin Sock <hbsock@gmail.com>
2019-04-19 22:24:00 -0400

    Add the random package,
    also add a quick function to print a random number between 1 and 6.

--- src/Lib.hs
+++ src/Lib.hs
@@ -7,12 +7,24 @@ module Lib
 
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
+import System.Random (getStdRandom, randomR)
+
+
+getRandomNumInRange :: Int -> Int -> IO Int
+getRandomNumInRange lo hi = getStdRandom (randomR (lo, hi))
 
 
 someFunc :: IO ()
 someFunc = do
-    response <- httpLBS =<< parseRequest ("https://en.wikipedia.org/wiki/" ++ "Anime")
 
+    rand <- getRandomNumInRange 1 6
+    print rand
+    
+    {--
+    response <- httpLBS =<< parseRequest ("https://en.wikipedia.org/wiki/" ++ "Anime")
     putStrLn $ "Status code was: " ++ show (getResponseStatusCode response)
     print $ getResponseHeader "Content-Type" response
     --L8.writeFile "wiki.html" $ getResponseBody response
+    --}
+
+    
