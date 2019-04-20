4217ab14a570f6d59040c9b28e587db6285ba991
Hanbin Sock <hbsock@gmail.com>
2019-04-19 20:18:31 -0400

    Move main logic to separate file.

--- app/Main.hs
+++ app/Main.hs
@@ -1,15 +1,9 @@
 module Main where
 
 
-import Parser (parseBoxNovelHTML)
+import Parser (parseMain)
 import System.IO
-import qualified Data.Text.Lazy.IO as L
 
 
 main :: IO ()
-main = do
-    file <- L.readFile "thing.html"
-    let chapter_contents = parseBoxNovelHTML file
-
-    L.writeFile "out.txt" $ chapter_contents
-    print chapter_contents
+main = parseMain
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,14 +1,22 @@
 module Parser 
-    ( parseBoxNovelHTML
+    ( parseMain
     ) where
 
 
 import Text.HTML.TagSoup
 import Data.Maybe (catMaybes)
 import qualified Data.Text.Lazy as T
+import qualified Data.Text.Lazy.IO as I
 
 parseForChapterContents :: [Tag T.Text] -> [Tag T.Text]
 parseForChapterContents = takeWhile (~/= "</div>") . dropWhile (~/= "<div class=\"text-left\">")
 
 parseBoxNovelHTML :: T.Text -> T.Text
 parseBoxNovelHTML = T.unlines . catMaybes . map maybeTagText . parseForChapterContents . parseTags
+
+parseMain :: IO ()
+parseMain = do
+    file <- I.readFile "thing.html"
+    let chapter_contents = parseBoxNovelHTML file
+
+    I.writeFile "out.txt" $ chapter_contents
