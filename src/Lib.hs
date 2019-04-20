899ab9c6aa6e5970c29ccf9abe8bb092f84eefab
Hanbin Sock <hbsock@gmail.com>
2019-04-19 22:18:28 -0400

    Remove some commented out code. comment out writing wikipedia content to file.

--- src/Lib.hs
+++ src/Lib.hs
@@ -8,19 +8,11 @@ module Lib
 import qualified Data.ByteString.Lazy.Char8 as L8
 import Network.HTTP.Simple
 
-import Control.Monad.IO.Class (MonadIO)
-
-
--- general_http :: MonadIO m => String -> m (Response L8.ByteString)
--- general_http verb = do
---     initReq <- parseRequest ("http://httpbin.org/" ++ verb)
---     httpLBS initReq
 
 someFunc :: IO ()
 someFunc = do
     response <- httpLBS =<< parseRequest ("https://en.wikipedia.org/wiki/" ++ "Anime")
 
-    putStrLn $ "Status code was: " ++ 
-                show (getResponseStatusCode response)
+    putStrLn $ "Status code was: " ++ show (getResponseStatusCode response)
     print $ getResponseHeader "Content-Type" response
-    L8.writeFile "wiki.html" $ getResponseBody response
+    --L8.writeFile "wiki.html" $ getResponseBody response
