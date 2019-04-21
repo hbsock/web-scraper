6bb269b4a3c65e3f0bbcaecb7c905d82b048c3e0
Hanbin Sock <hbsock@gmail.com>
2019-04-21 15:20:23 -0400

    Get list of urls

--- app/Main.hs
+++ app/Main.hs
@@ -12,6 +12,9 @@ main = do
     let output_dir = "output/"
     let output_file_names = map (\t -> output_dir ++ t ++ ".txt") chap_strs
 
+    let urls = map (\t -> "https://boxnovel.com/novel/chaotic-sword-god/" ++ t) chap_strs
+
     print output_file_names
+    print urls
     -- scrapeChaoticSwordGodNovel "https://boxnovel.com/novel/chaotic-sword-god/chapter-1/" "chapter-1.txt"
 
