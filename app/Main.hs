995a9edbbdab2663658c26db89e5603faf41ae23
Hanbin Sock <hbsock@gmail.com>
2019-04-20 22:51:55 -0400

    Add initial code to start scraping for each chapter

--- app/Main.hs
+++ app/Main.hs
@@ -3,8 +3,15 @@ module Main where
 
 import WebScraper (scrapeChaoticSwordGodNovel)
 
+
 main :: IO ()
-main = scrapeChaoticSwordGodNovel 
-        "https://boxnovel.com/novel/chaotic-sword-god/chapter-1/" 
-        "chapter-1.txt"
+main = do
+    let chap_nums = [1..10]
+    let chap_strs = map (\n -> "chapter-" ++ (show n)) chap_nums
+
+    let output_dir = "output/"
+    let output_file_names = map (\t -> output_dir ++ t ++ ".txt") chap_strs
+
+    print output_file_names
+    -- scrapeChaoticSwordGodNovel "https://boxnovel.com/novel/chaotic-sword-god/chapter-1/" "chapter-1.txt"
 
