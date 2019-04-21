fc7264814f0970a4b76963b67a44918ac6a1e26f
Hanbin Sock <hbsock@gmail.com>
2019-04-21 15:42:45 -0400

    Move main function logic to separate function.

--- app/Main.hs
+++ app/Main.hs
@@ -4,21 +4,30 @@ module Main where
 import WebScraper (scrapeChaoticSwordGodNovel)
 import System.Directory (createDirectoryIfMissing)
 
+scrapeMain :: IO ()
+scrapeMain = do
+    let chap_nums = [1..3]
+    let output_dir = "output/"
+
+    let chap_strs = map (\n -> "chapter-" ++ (show n)) chap_nums
+    let output_file_names = map (\t -> output_dir ++ t ++ ".txt") chap_strs
+    let urls = map (\t -> "https://boxnovel.com/novel/chaotic-sword-god/" ++ t) chap_strs
+
+    createDirectoryIfMissing True output_dir
+    let output_name_and_urls = zip urls output_file_names 
+    
+    return ()
 
 
 main :: IO ()
 main = do
-    let chap_nums = [1..10]
-    let chap_strs = map (\n -> "chapter-" ++ (show n)) chap_nums
-
-    let output_dir = "output/"
-    let output_file_names = map (\t -> output_dir ++ t ++ ".txt") chap_strs
-
-    let urls = map (\t -> "https://boxnovel.com/novel/chaotic-sword-god/" ++ t) chap_strs
-
-    createDirectoryIfMissing True output_dir
-
-
+    scrapeMain
+{--
+    mapM_ 
+        (\(url, output_file) -> createDirectoryIfMissing url output_file) 
+        output_name_and_urls 
+        --}
+    -- print output_name_and_urls
     -- print output_file_names
     -- print urls
     -- scrapeChaoticSwordGodNovel "https://boxnovel.com/novel/chaotic-sword-god/chapter-1/" "chapter-1.txt"
