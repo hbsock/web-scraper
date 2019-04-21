a8677b36ce288b94bbb110a350dbf95af3a37b26
Hanbin Sock <hbsock@gmail.com>
2019-04-21 17:54:10 -0400

    Add a general parse function.

--- app/Main.hs
+++ app/Main.hs
@@ -2,12 +2,22 @@ module Main where
 
 
 import WebScraper (scrapeMain)
-import Parser (parseChaoticSwordGodHTML)
+import Parser
+
+import qualified Data.Text.Lazy.IO as I
+
+testParsing :: IO ()
+testParsing = do
+    content <- I.readFile "the-kings-avatar-example.html"
+    I.writeFile "tmp.txt" $ parseBoxnovel content
 
 
 main :: IO ()
 main = do
+    testParsing
+{--
     scrapeMain  [41..50] 
                 "output/chaotic-sword-god/" 
                 "https://boxnovel.com/novel/chaotic-sword-god/"
                 parseChaoticSwordGodHTML
+--}
--- src/Parser.hs
+++ src/Parser.hs
@@ -1,15 +1,39 @@
 module Parser 
-    ( parseChaoticSwordGodHTML
+    ( 
+        parseChaoticSwordGodHTML,
+        parseTheKingsAvatarHTML,
+        parseBoxnovel
     ) where
 
 
 import Text.HTML.TagSoup
 import Data.Maybe (catMaybes)
+import Data.List (intercalate)
 import qualified Data.Text.Lazy as T
-import qualified Data.Text.Lazy.IO as I
+
+
+parseBoxnovel :: T.Text -> T.Text
+parseBoxnovel = 
+    T.unlines .
+    takeWhile (/= (T.pack "© 2018 BoxNovel. All rights reserved" )) .
+    catMaybes . 
+    map maybeTagText .
+    concat .
+    map (take 2) .
+    sections (~== "<p>") .
+    dropWhile (~/= "<div class=\"text-left\">") . parseTags
+
 
 parseForChapterContents :: [Tag T.Text] -> [Tag T.Text]
 parseForChapterContents = takeWhile (~/= "</div>") . dropWhile (~/= "<div class=\"text-left\">")
 
+
 parseChaoticSwordGodHTML :: T.Text -> T.Text
 parseChaoticSwordGodHTML = T.strip . T.unlines . catMaybes . map maybeTagText . parseForChapterContents . parseTags
+
+
+
+
+parseTheKingsAvatarHTML :: T.Text -> [Tag T.Text]
+parseTheKingsAvatarHTML = takeWhile (~/= "</div>") . dropWhile (~/= "<div class=\"text-left\">") . parseTags
+
