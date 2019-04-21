8e09c8827a36e3d5307b973d866c20669b8a5b71
Hanbin Sock <hbsock@gmail.com>
2019-04-21 15:56:21 -0400

    Move scrapeMain to WebScraper.hs

--- app/Main.hs
+++ app/Main.hs
@@ -1,33 +1,10 @@
 module Main where
 
 
-import WebScraper (scrapeChaoticSwordGodNovel)
-import System.Directory (createDirectoryIfMissing)
+import WebScraper (scrapeMain)
 
-scrapeMain :: [Int] -> FilePath -> IO ()
-scrapeMain chap_nums output_dir = do
-
-    let chap_strs = map (\n -> "chapter-" ++ (show n)) chap_nums
-    let output_file_names = map (\t -> output_dir ++ t ++ ".txt") chap_strs
-    let urls = map (\t -> "https://boxnovel.com/novel/chaotic-sword-god/" ++ t) chap_strs
-
-    createDirectoryIfMissing True output_dir
-    let output_name_and_urls = zip urls output_file_names 
-    
-    mapM_ (\(url, out) -> scrapeChaoticSwordGodNovel url out) output_name_and_urls
 
 
 main :: IO ()
 main = do
-    scrapeMain [1..10] "output/chaotic-sword-god/"
-
-{--
-    mapM_ 
-        (\(url, output_file) -> createDirectoryIfMissing url output_file) 
-        output_name_and_urls 
-        --}
-    -- print output_name_and_urls
-    -- print output_file_names
-    -- print urls
-    -- scrapeChaoticSwordGodNovel "https://boxnovel.com/novel/chaotic-sword-god/chapter-1/" "chapter-1.txt"
-
+    scrapeMain [31..40] "output/chaotic-sword-god/"
--- src/WebScraper.hs
+++ src/WebScraper.hs
@@ -1,6 +1,6 @@
 module WebScraper 
     (
-        scrapeChaoticSwordGodNovel
+        scrapeMain
     ) where
 
 
@@ -8,6 +8,7 @@ import Parser (parseChaoticSwordGodHTML)
 import Tools (getWebsiteWithDelay)
 import qualified Data.Text.Lazy.IO as I
 import Data.Text.Lazy.Encoding (decodeUtf8)
+import System.Directory (createDirectoryIfMissing)
 
 
 scrapeChaoticSwordGodNovel :: String -> String -> IO ()
@@ -17,3 +18,16 @@ scrapeChaoticSwordGodNovel url output_file = do
     
     putStrLn $ "Writing file: " ++ output_file
     I.writeFile output_file $ parsed_contents
+
+
+scrapeMain :: [Int] -> FilePath -> IO ()
+scrapeMain chap_nums output_dir = do
+
+    let chap_strs = map (\n -> "chapter-" ++ (show n)) chap_nums
+    let output_file_names = map (\t -> output_dir ++ t ++ ".txt") chap_strs
+    let urls = map (\t -> "https://boxnovel.com/novel/chaotic-sword-god/" ++ t) chap_strs
+
+    createDirectoryIfMissing True output_dir
+    let output_name_and_urls = zip urls output_file_names 
+    
+    mapM_ (\(url, out) -> scrapeChaoticSwordGodNovel url out) output_name_and_urls
