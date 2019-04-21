4eb6805cf0a84bc6a9cb815104c7b6cffe467555
Hanbin Sock <hbsock@gmail.com>
2019-04-21 15:24:25 -0400

    Add usage of directory library to create dirs.

--- app/Main.hs
+++ app/Main.hs
@@ -2,6 +2,8 @@ module Main where
 
 
 import WebScraper (scrapeChaoticSwordGodNovel)
+import System.Directory (createDirectoryIfMissing)
+
 
 
 main :: IO ()
@@ -14,7 +16,10 @@ main = do
 
     let urls = map (\t -> "https://boxnovel.com/novel/chaotic-sword-god/" ++ t) chap_strs
 
-    print output_file_names
-    print urls
+    createDirectoryIfMissing True output_dir
+
+
+    -- print output_file_names
+    -- print urls
     -- scrapeChaoticSwordGodNovel "https://boxnovel.com/novel/chaotic-sword-god/chapter-1/" "chapter-1.txt"
 
