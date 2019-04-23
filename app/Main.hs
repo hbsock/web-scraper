77043220733e389981b73e2f5916d6fdb58f891f
Hanbin Sock <hbsock@gmail.com>
2019-04-22 22:49:30 -0400

    Add a test function to run in main to test out exceptions.

--- app/Main.hs
+++ app/Main.hs
@@ -1,19 +1,49 @@
 module Main where
 
 
+
+import Control.Exception
+import Data.Typeable (Typeable)
+import qualified Data.Text.Lazy.IO as I
+import qualified Data.Text as T
+
 import WebScraper (scrapeMain)
 import Parser
-import qualified Data.Text.Lazy.IO as I
+
+data WebScraperException = 
+    InvalidNumberRange !T.Text
+    deriving (Show, Typeable)
+
+
+instance Exception WebScraperException
+
+
+testExceptions :: IO ()
+testExceptions = do
+    let low = 10
+    let high = 5
+    case compare low high of
+        LT -> putStrLn "Low is less than high, that is okay."
+        EQ -> putStrLn "Low is equal to high, that is okay."
+        GT -> throw ( 
+                InvalidNumberRange 
+                (T.pack "The lower number is higher than the high number."))
+        
+
 
 testParsing :: IO ()
 testParsing = do
     content <- I.readFile "the-kings-avatar-example.html"
     I.writeFile "tmp.txt" $ parseBoxnovel content
 
+    
 
 main :: IO ()
 main = do
+    testExceptions
+{--
     scrapeMain  [1..1500] 
                 "output/the-kings-avatar/" 
                 "https://boxnovel.com/novel/the-kings-avatar/"
                 parseBoxnovel
+--}
