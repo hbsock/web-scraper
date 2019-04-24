d686467f566ceaad930e4df09a0df693c840fe63
Hanbin Sock <hbsock@gmail.com>
2019-04-23 22:20:15 -0400

    Add input data type and make a test input function that will confirm inputs are properly formatted.

--- app/Main.hs
+++ app/Main.hs
@@ -18,8 +18,18 @@ data WebScraperException =
 instance Exception WebScraperException
 
 
-testExceptions :: Integer -> Integer -> IO ()
-testExceptions low high = do
+data Inputs = Inputs {
+    low :: Integer,
+    high :: Integer
+} deriving (Eq, Ord, Show)
+
+
+testInputs :: Inputs -> IO ()
+testInputs inputs = do
+    testLowAndHigh (low inputs) (high inputs)
+
+testLowAndHigh :: Integer -> Integer -> IO ()
+testLowAndHigh low high = do
     case compare low high of
         LT -> putStrLn "Low is less than high, that is okay."
         EQ -> putStrLn "Low is equal to high, that is okay."
@@ -34,11 +44,16 @@ testParsing = do
 
     
 
+
 main :: IO ()
 main = do
-    testExceptions 5 10
-    testExceptions 1 1
-    testExceptions 10 5
+    let inputs = Inputs {
+        low = 1,
+        high = 10
+    }
+
+    testInputs inputs
+
 {--
     scrapeMain  [1..1500] 
                 "output/the-kings-avatar/" 
