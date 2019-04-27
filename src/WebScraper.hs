cbb0f38417f9ff43e8b008e716f540936e98a454
Hanbin Sock <hbsock@gmail.com>
2019-04-27 12:03:46 -0400

    Rename scrapeNovel function.

--- src/WebScraper.hs
+++ src/WebScraper.hs
@@ -11,8 +11,8 @@ import Data.Text.Lazy.Encoding (decodeUtf8)
 import System.Directory (createDirectoryIfMissing)
 
 
-scrapeNovel :: String -> String -> (T.Text -> T.Text) -> IO ()
-scrapeNovel url output_file parse_func = do
+scrapeAndParseURL :: String -> String -> (T.Text -> T.Text) -> IO ()
+scrapeAndParseURL url output_file parse_func = do
     contents <- getWebsiteWithDelay url
     let parsed_contents = parse_func $ decodeUtf8 contents
     
@@ -26,8 +26,9 @@ scrapeMain chap_nums output_dir base_url parse_func = do
     let chap_strs = map (\n -> "/chapter-" ++ (show n)) chap_nums
     let output_file_names = map (\t -> output_dir ++ t ++ ".txt") chap_strs
     let urls = map (\t -> base_url ++ t) chap_strs
+    let output_name_and_urls = zip urls output_file_names 
 
     createDirectoryIfMissing True output_dir
-    let output_name_and_urls = zip urls output_file_names 
     
-    mapM_ (\(url, out) -> scrapeNovel url out parse_func) output_name_and_urls
+    mapM_ (\(url, out) -> scrapeAndParseURL url out parse_func) 
+        output_name_and_urls
