8892975d33c774ccc98e221854822052703e0263
Hanbin Sock <hbsock@gmail.com>
2019-05-22 23:22:56 -0400

    Add property of checking for valid inputs.

--- test/Spec.hs
+++ test/Spec.hs
@@ -1,5 +1,8 @@
 import Test.QuickCheck
 import CmdParser (isInputInvalid, Inputs (..))
+import System.FilePath (isValid)
+import Network.URI (isURI)
+
 
 instance Arbitrary Inputs where
     arbitrary = Inputs <$> arbitrary 
@@ -9,7 +12,14 @@ instance Arbitrary Inputs where
         <*> arbitrary
 
 prop_input :: Inputs -> Bool
-prop_input input = True
+prop_input inputs = case isInputInvalid inputs of
+        Just(_) -> invalid_cond == True
+        Nothing -> invalid_cond == False
+    where invalid_cond = or [ (low inputs) > (high inputs),
+                            (low inputs) < 0,
+                            (high inputs) < 0,
+                            not . isValid $ (output_dir inputs),
+                            not . isURI $ (base_url inputs)]
 
 main :: IO ()
 main = do
